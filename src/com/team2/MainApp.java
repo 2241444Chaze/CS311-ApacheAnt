e364d2f60cdb02904979dfb37503bc54450a72e7
Brent Menos <2241577@slu.edu.ph>
2025-10-24 18:46:38 +0800

    Changed the location of the instantiated classes

--- src/com/team2/MainApp.java
+++ src/com/team2/MainApp.java
@@ -5,15 +5,19 @@ import com.team2.solutions.lists.LinkedListSolutions;
 import com.team2.solutions.math.MathSolutions;
 import com.team2.solutions.strings.StringSolutions;
 
+import java.util.Arrays;
+
 /**
  * The main entry point for the team's project.
  */
 public class MainApp {
-    ArraySolutions arraysolutions = new ArraySolutions();
-    LinkedListSolutions linkedListSolutions = new LinkedListSolutions();
-    MathSolutions mathsolutions = new MathSolutions();
-    StringSolutions stringsolutions = new StringSolutions();
+
     public static void main(String[] args) {
+        ArraySolutions arraysolutions = new ArraySolutions();
+        LinkedListSolutions linkedListSolutions = new LinkedListSolutions();
+        MathSolutions mathsolutions = new MathSolutions();
+        StringSolutions stringsolutions = new StringSolutions();
+
     }
 }
 
